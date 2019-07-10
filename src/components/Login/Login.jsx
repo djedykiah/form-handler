9572d3b920ee21a74f43020f38446be4f97797e2
Admin_Tony <admin_tony@iMac-Admin-Tony.local>
2019-07-10 18:08:41 +0300

    FormInput component added

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react'; 
+
 import FormLegend from '../FormLegend';
+import FormInput from '../FormInput';
 
+export default class Login extends Component {
+	state = {
+		name: '',
+	}
 
-const Login = () => {
-	return (
-		<div>
-			<FormLegend  value={'Вход в систему'}/>
+	onChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+	render() { 
+		const { name } = this.state;
+		return (
+			<div>
+				<FormLegend  value="Вход в систему"/> 
+				<FormInput
+					label="name"
+					name="name"
+					errorMessage = "invalid name"
+					value = {name} 
+					onChange = {this.onChange}
+				/>
+
 		</div>
-	)
-} 
+		)
+	}
+}
 
-export default Login;
